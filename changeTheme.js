a5f87ae172a4c248dbcc5fd6591f8d2ea7e3dbc5
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2023-10-15 17:03:42 +0200

    remove she/her

--- changeTheme.js
+++ changeTheme.js
@@ -1,7 +1,7 @@
 const friendlyCactus = document.querySelector("#friendlyCactus");
 const angryPomeranian = document.querySelector("#angryPomeranian");
 const brownPurple = document.querySelector("#brownPurple");
-const warhammer = document.querySelector("#warhammer");
+const warhammer = document.querySelector("#warhammerButton");
 const sunFlower = document.querySelector("#sunFlower");
 const darkHallow = document.querySelector("#darkHallow");
 const kvist = document.querySelector("#kvist");
@@ -10,3 +10,8 @@ const cloudberry = document.querySelector("#cloudberry");
 
 const demoButton = document.querySelector("#demo-button");
 const codeButton = document.querySelector("#code-button");
+const body = document.querySelector("body"); 
+
+warhammer.addEventListener("click", function(){
+    body.style.backgroundColor = "#F6C47E";
+})
