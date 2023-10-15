feb4ec6efabfb2b1f041047ff18f618b5cf102d7
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2023-10-15 16:56:49 +0200

    Add colored buttons

---
+++ changeTheme.js
@@ -0,0 +1,12 @@
+const friendlyCactus = document.querySelector("#friendlyCactus");
+const angryPomeranian = document.querySelector("#angryPomeranian");
+const brownPurple = document.querySelector("#brownPurple");
+const warhammer = document.querySelector("#warhammer");
+const sunFlower = document.querySelector("#sunFlower");
+const darkHallow = document.querySelector("#darkHallow");
+const kvist = document.querySelector("#kvist");
+const cloudberry = document.querySelector("#cloudberry");
+
+
+const demoButton = document.querySelector("#demo-button");
+const codeButton = document.querySelector("#code-button");
