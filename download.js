553a9d0af8b2afb5084e12e29465caf2ee212658
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2023-11-02 12:19:14 +0100

    Add download

---
+++ download.js
@@ -0,0 +1,11 @@
+// const button = document.querySelector("#downloadButton"); 
+// const pdfURL = "https://drive.google.com/uc?export=download&id=1__Sr0v9-HflUSprQ4SeaNh663YVNAitx";
+
+// button.addEventListener("click", function(event) {
+//     event.preventDefault(); 
+//     console.log(pdfURL);
+//     button.href = pdfURL; 
+//     button.download = "Ingeborg Sanna CV.pdf"; 
+//     button.target = "_blank"; 
+//     button.click();
+// })
